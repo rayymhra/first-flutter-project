8d7e90a4bbec306eaf7c3315a0ff7a8f750ab673
rayymhra <rayraeilish@gmail.com>
2025-08-04 21:19:05 +0700

    pink navbar, svg package, black square icon

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          'halooo welcomee', 
+          style: TextStyle(
+            color: Colors.black, 
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        backgroundColor: Colors.pink,
+        centerTitle: true, //to make the text to be in the center
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10)
+          )
+        ),
+      ),
     );
   }
 }
